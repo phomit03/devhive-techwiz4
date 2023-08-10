52ad0205b853c997d58e8f44dae9b492b8740a5c
nguyenvietanh1409 <103101142+nguyenvietanh1409@users.noreply.github.com>
2023-08-10 18:46:59 +0700

    Revert "call api"

--- frontend/admin/src/app/response/login-response.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface UserResponse {
-    id: number;
-    username: string;
-    email: string;
-    roles: string[];
-    accessToken: string;
-    tokenType: string;
-  }
--- frontend/admin/src/enviroments/enviroment.ts
+++
@@ -1,7 +0,0 @@
-const DOMAIN = `http://localhost:8080`;
-const API = `api`;
-
-export const environment = {
-  production: false,
-  baseApiUrl: `${DOMAIN}/${API}`,
-};
