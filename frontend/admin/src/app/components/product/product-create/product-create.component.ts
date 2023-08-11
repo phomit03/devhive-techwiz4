4d324d5f49bc28f3a21e438b45248acd026f8d1a
nguyenvietanh <vietanh20030914@gmail.com>
2023-08-12 01:06:52 +0700

    fe

--- frontend/admin/src/app/components/product/product-create/product-create.component.ts
+++ frontend/admin/src/app/components/product/product-create/product-create.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Category } from 'src/app/response/category-response.interface';
 import { MessageResponse } from 'src/app/response/messageResponse.interface';
 import { Product } from 'src/app/response/response.interface';
+import { CategoryService } from 'src/app/services/category/category.service';
 import { ProductService } from 'src/app/services/product/product.service';
 import Swal from 'sweetalert2';
 
@@ -25,12 +27,23 @@ export class ProductCreateComponent {
     status: 0
   }
 
+  categories: Category[] = []
+
   selectedImage: File | null = null; 
 
   constructor(
     private router: Router,
-    private services: ProductService
+    private services: ProductService,
+    private categoryService: CategoryService
   ) {}
+
+  ngOnInit() {
+    this.categoryService.getAll().subscribe({
+      next: (response) => {
+        this.categories = response;
+      }
+    })
+  }
 
   create() {
     if (!this.selectedImage) {
